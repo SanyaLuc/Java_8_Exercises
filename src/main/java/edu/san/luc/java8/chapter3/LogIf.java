8df6a587c8432a71f1c18685a9ed29be7febbcd8
SanyaLuc <alex.m.kolpakov@gmail.com>
2015-12-14 08:59:21 +0200

    implemented logging with java.util.logging API

--- src/main/java/edu/san/luc/java8/chapter3/LogIf.java
+++ src/main/java/edu/san/luc/java8/chapter3/LogIf.java
@@ -1,7 +1,10 @@
 package edu.san.luc.java8.chapter3;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -10,11 +13,23 @@ import java.util.logging.Logger;
  */
 public class LogIf {
     private Logger log = Logger.getLogger(getClass().getName());
-    public static void main(String[] args) throws IOException {
-        InputStream in = LogIf.class.getResourceAsStream("/logging.properties");
-        LogManager.getLogManager().readConfiguration(in);
+    public static void main(String[] args) throws IOException, URISyntaxException {
+//        InputStream in = LogIf.class.getResourceAsStream("/logging.properties");
+//        LogManager.getLogManager().readConfiguration(in);
+//
+        File file = new File(ClassLoader.getSystemResource("logging.properties").toURI());
+        System.setProperty("java.util.logging.config.file", file.getAbsolutePath());
+        LogManager.getLogManager().readConfiguration();
 
-        //System.setProperty("java.util.logging.config.file", "");
+        LogIf logIf = new LogIf();
+        logIf.log.severe("severe");
+        logIf.log.warning("warning");
+        logIf.log.info("info");
+        logIf.log.config("config");
+        logIf.log.fine("fine");
+        logIf.log.finer("finer");
+        logIf.log.finest("finest");
+        logIf.log.log(Level.ALL, "log ALL");
     }
 
 
